10830d1bbd712958a895b53645e5241e07fc1a16
CibelleSousa <cibelle.rodrigues@outlook.com>
2025-10-28 19:20:59 -0300

    refactor: Expande estrutura de dados para novas telas
    Atualiza o tipo ServiceProvider, preenche os 100 itens com dados completos, cria o tipo e mock para UserProfile e atualiza a documentação no README.md. Closes #10

---
+++ src/data/mockedUser.ts
@@ -0,0 +1,8 @@
+import { UserProfile } from "./mockedUser.type";
+
+
+export const userMockData: UserProfile = {
+  id: 'u1',
+  nome: 'João Silva',
+  urlFoto: require('../../assets/user/user-logado.png')
+};
---
+++ src/data/mockedUser.type.ts
@@ -0,0 +1,5 @@
+export type UserProfile = {
+  id: string;
+  nome: string;
+  urlFoto: number;
+};
