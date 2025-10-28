7ecb22b900a8c10675800b7424ba9d9f452b3542
omniberto <ehmcerqueira.cic@uesc.br>
2025-10-28 18:39:52 -0300

    saving info

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,6 +1,34 @@
 import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import RenderedText from "../../components/RenderedComponents/RenderedText";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function HomeScreen() {
+export default function Profile() {
+
+    return (
+        <View>
+            <View style = {styles.head}>
+
+            </View>
+            <View style= {styles.menuOption}>
+                <FontAwesome style = {styles.icon}/>
+                <RenderedText style={styles.menuText}>
+                    Opt
+                </RenderedText>
+                <FontAwesome style = {styles.button}/>
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    head: {alignContent:'center',
+            justifyContent: 'center'
+    },
+    profilePic: {height: 500, width: 500, aspectRatio: 1/1},
+    profileName: {fontFamily:'LeagueSpartan_600SemiBold'},
+    menuOption: {},
+    button: {},
+    icon: {},
+    menuText: {fontFamily: 'LeagueSpartan_400Regular'}    
+})
